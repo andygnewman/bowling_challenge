8ceff363d354b546accca0e3ccb9a68f6542c67b
Andy <andygnewman@gmail.com>
2015-02-02 13:42:56 +0000

    logic all working through interface ad tests all green

--- js/interface.js
+++ js/interface.js
@@ -1,30 +1,34 @@
 console.log("hello world");
 
-var game = new Game();
+  var scoreboard = new Scoreboard();
+  var refreshscores = new RefreshScores();
+  var rollTracker = new RollTracker();
+  var scoreBoardTable = new ScoreBoardTable();
+
+  var populateMaxScoreDropDown = function() {
+    maxScoreDropDown = "";
+    for (var i = 0; i <= this.rollTracker.tracker.maxRollScore; i++) {
+    maxScoreDropDown += "<option value=" + i + ">" + i + "</option>";
+    }
+    return maxScoreDropDown; 
+  }
 
   $(document).ready(function() {
-  game.populateScoreBoard();
-  game.populateMaxScoreDropDown();
-  $('#score-board').html($(game.scoreBoard));
-  $('#score-dropdown').html($(game.maxScoreDropDown));
-  $('#frame-number').text(game.frame.rollTracker.frameNumber);
-  $('#ball-number').text(game.frame.rollTracker.rollNumber);
+  $('#score-dropdown').html($(populateMaxScoreDropDown()));
+  $('#frame-number').text(rollTracker.tracker.frameNumber);
+  $('#ball-number').text(rollTracker.tracker.rollNumber);
 
   
   $('#score-entry').on('submit', function(event) {
   event.preventDefault();
-  var bowl = $("#score-dropdown option:selected").val();
-  game.frame.updateRollScore(bowl);
-  game.frame.updateFrameWithScoreAndAdvanceRoll();
-  game.populateScore();
-  game.populateScoreBoard();
-  game.populateMaxScoreDropDown();
-  $('#score-board').html($(game.scoreBoard)); 
-  $('#score-dropdown').html($(game.maxScoreDropDown));
-  $('#frame-number').text(game.frame.rollTracker.frameNumber);
-  $('#ball-number').text(game.frame.rollTracker.rollNumber); 
+  var bowlScore = parseInt($("#score-dropdown option:selected").val());
+  scoreboard.addBowlToScoreBoard(bowlScore, rollTracker.tracker.frameNumber, rollTracker.tracker.rollNumber);
+  rollTracker.advanceRoll(bowlScore);
+  refreshscores.refreshCumulativeScores(scoreboard.board);
+  $('#score-board').html($(scoreBoardTable.returnScoreBoardHTML(scoreboard.board))); 
+  $('#score-dropdown').html($(populateMaxScoreDropDown()));
+  $('#frame-number').text(rollTracker.tracker.frameNumber);
+  $('#ball-number').text(rollTracker.tracker.rollNumber); 
   });
-
-
 
 });
--- js/scoreboardtable.js
+++ js/scoreboardtable.js
@@ -1,18 +1,18 @@
 var ScoreBoardTable = function() {
 
-
-
   ScoreBoardTable.prototype.returnScoreBoardHTML = function(board) {
     scoreBoardHTML = "<tr><td>Frame</td>";
-    for (var property in board[0]) {
-      scoreBoardHTML += "<td>" + property +"</td>"; 
+    properties = Object.keys(board[0]).sort();
+    for (var z = 0; z < properties.length; z++) {
+      scoreBoardHTML += "<td>" + properties[z] +"</td>"; 
     }
     scoreBoardHTML += "</tr>";
     for (var i = 0; i < board.length; i++) {
       scoreBoardHTML += "<tr><td>" + (i + 1) + "</td>";
       frame = board[i];
-      for (var property in frame) {
-        scoreBoardHTML += "<td>" + frame[property] + "</td>";
+      properties = Object.keys(frame).sort();
+      for (var x = 0; x < properties.length; x++) {
+        scoreBoardHTML += "<td>" + frame[properties[x]] + "</td>";
       } 
       scoreBoardHTML += "</tr>";
     }
